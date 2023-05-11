600901b41afbc393005fc65cf2942561832ffa31
Andrei Voicu <dreivoicu@gmail.com>
2023-05-11 14:44:09 +0300

    save

---
+++ src/ui/common/components/DataTable/data-table.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="list">
+    <div class="rows"></div>
+  </div>
+</template>
+
+<style lang="scss" module></style>
--- src/ui/views/Dashboard.vue
+++ src/ui/views/Dashboard.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { DataTable } from "@/ui/common";
+</script>
 
 <template>
   <header>
     <h1>Table</h1>
   </header>
+
+  <main :class="$style.main">
+    <data-table :class="$style.cover" />
+  </main>
 </template>
 
-<style lang="scss" module></style>
+<style lang="scss" module>
+@import "@/ui/assets/styles/abstracts";
+
+.main {
+  flex: 1;
+  position: relative;
+  background-color: var(--surface);
+}
+
+.cover {
+  @include absolute(0, 0);
+}
+</style>
